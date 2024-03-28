a774c9aa864417d79a7828cdf375cc6a535e40e6
Alex <alexander.mcisaac@student.tdsb.on.ca>
2024-03-28 11:29:48 -0400

    windows movie maker demo

---
+++ src/mainFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+//import javax class package 
+
+public class mainFrame extends JFrame{
+    public void init(){
+        setTitle("windows movie maker 2000");
+        setSize(600, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
